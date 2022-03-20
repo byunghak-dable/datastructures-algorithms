372ae6bc1900cc67c450509a9e4d1deb4b532558
weed082 <forsythia3240@gmail.com>
2022-03-20 19:22:15 +0900

    [SORT]

--- datastructure/java/app/src/main/java/algo/tree/TreeInterface.java
+++ datastructure/java/app/src/main/java/algo/tree/TreeInterface.java
@@ -2,9 +2,14 @@ package algo.tree;
 
 public interface TreeInterface<T> {
   boolean isEmpty();
+
   int size();
+
   boolean add(T data); // if exist return false
+
   boolean remove(T data);
+
   boolean contains(T data);
+
   int height();
 }
