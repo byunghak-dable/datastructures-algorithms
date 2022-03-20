4334fc3aeed2894f9317a94ff395b7cb805fbcf1
weed082 <forsythia3240@gmail.com>
2022-03-20 16:59:43 +0900

    [UPDATE]

--- datastructure/java/app/src/main/java/algo/App.java
+++ datastructure/java/app/src/main/java/algo/App.java
@@ -1,6 +1,5 @@
 package algo;
 
-import algo.stack.ListStack;
 import algo.tree.BinarySearchTree;
 import algo.tree.TreeTraversalOrder;
 import java.util.Iterator;
@@ -8,24 +7,8 @@ import java.util.Iterator;
 // binary heap => PQueue => Union Find
 public class App {
   public static void main(String[] args) {
-    // SinglyLinkedList<String> singlyList = new SinglyLinkedList<String>();
-    // singlyList.addLast("yes1");
-    // singlyList.addLast("yes2");
-    // singlyList.addLast("yes3");
-    // singlyList.addLast("yes4");
-    // singlyList.addFirst("first1");
-    // singlyList.addFirst("first2");
-    // ListStack<String> stack = new ListStack<String>();
-    // System.out.println(stack.isEmpty());
-    // stack.push("yes");
-    // stack.push("yes1");
-    // stack.push("yes2");
-    // for (String string : stack) {
-    // System.out.println(string);
-    //
-    // }
     BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
-          bst.add(5);
+    bst.add(5);
     bst.add(1);
     bst.add(7);
     bst.add(2);
@@ -34,5 +17,4 @@ public class App {
       System.out.println(iter.next());
     }
   }
-
 }
