e5765596e7cb714e7ec0858a2333d564a81f8d47
weed082 <forsythia3240@gmail.com>
2022-06-03 15:21:26 +0900

    [UPDATE] dp 알고리즘 기초(코인)

--- algo/dp/coin.py
+++ algo/dp/coin.py
@@ -21,16 +21,19 @@ def caseCoinChange1(coins: list[int], amount: int) -> int:
 
 # 모든 코인의 경우의 수 - 공간 복잡도 O(n)
 def caseCoinChange2(coins: list[int], amount: int) -> int:
-    dp = [[1 if i == 0 else 0] * (len(coins) + 1) for i in range(amount + 1)]
-    for i in range(1, amount + 1):
-        for j in range(len(coins) - 1, -1, -1):
-            dp[i][j] = dp[i][j + 1]
-            if i >= coins[j]:
-                dp[i][j] += dp[i - coins[j]][j]
-    return dp[-1][0]
+    dp = [1] + [0] * amount
+    for i in range(len(coins) - 1, -1, -1):
+        nDp = [1] + [0] * amount
+        for j in range(1, amount + 1):
+            nDp[j] = dp[j]
+            if j >= coins[i]:
+                nDp[j] += nDp[j - coins[i]]
+        dp = nDp
+    return dp[-1]
 
 
 coins = [1, 2, 5]
 amount = 11
-print(countCoinChange(coins, amount))
+# print(countCoinChange(coins, amount))
 print(caseCoinChange1(coins, 5))
+print(caseCoinChange2(coins, 5))
