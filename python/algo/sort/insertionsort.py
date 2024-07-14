766a0410ac14be876d86740a68ee5cd04206a241
byunghak082 <byunghak@dable.io>
2024-07-14 16:11:45 +0900

    [FIX] start from 1 instead of 0

--- python/algo/sort/insertionsort.py
+++ python/algo/sort/insertionsort.py
@@ -1,11 +1,11 @@
 def sort(input_arr: list[int]) -> list[int]:
     arr = input_arr[:]
 
-    for i in range(len(arr) - 1):
+    for i in range(1, len(arr)):
         j = i
 
-        while j >= 0 and arr[j] > arr[j + 1]:
-            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        while j > 0 and arr[j - 1] > arr[j]:
+            arr[j - 1], arr[j] = arr[j], arr[j - 1]
             j -= 1
 
     return arr
