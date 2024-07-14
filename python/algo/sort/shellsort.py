0096516ca877d8356bbcb3e9bd84118dcddd8d27
byunghak082 <byunghak@dable.io>
2024-07-14 16:02:10 +0900

    [UPDATE] add shell sort

---
+++ python/algo/sort/shellsort.py
@@ -0,0 +1,25 @@
+def sort(input_arr: list[int]) -> list[int]:
+    arr = input_arr[:]
+    n = len(arr)
+    gap = n // 2
+
+    while gap > 0:
+        for i in range(gap, n):
+            temp = arr[i]
+            j = i
+
+            while j >= gap and arr[j - gap] > temp:
+                arr[j] = arr[j - gap]
+                j -= gap
+            arr[j] = temp
+
+        gap //= 2
+
+    return arr
+
+
+arr = [8, 5, 6, 2, 4]
+sorted_arr = sort(arr)
+
+print("original", arr)
+print("sorted", sorted_arr)
